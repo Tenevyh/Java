ca9cab199a14f24e23642a673126961152a756fe
Tenevyh <tenevyh@gmail.com>
2021-10-30 11:56:50 +0300

    Create Solution.java

---
+++ JavaR/12lvl/Task11.2/Solution.java
@@ -0,0 +1,19 @@
+/*
+Земля: техническая характеристика
+Сделай все переменные класса Planet не статическими.
+После этого внеси необходимые правки в метод main.
+
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Planet earth = new Planet();
+        earth.name = "Земля";
+        earth.age = 4_540_000_000L;
+        earth.speed = 170_218;
+        earth.area = 510_072_000;
+        earth.printInformation();
+    }
+}
