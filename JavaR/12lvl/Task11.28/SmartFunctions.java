577e064cd6cf53733d0d43cd7278cad55f199af6
Tenevyh <tenevyh@gmail.com>
2021-11-04 12:47:38 +0300

    Create SmartFunctions.java

---
+++ JavaR/12lvl/Task11.28/SmartFunctions.java
@@ -0,0 +1,7 @@
+/*
+Телефон в наследство
+Необходимо правильно расставить ключевые слова extends и implements в представленных классах, чтобы программа компилировалась.
+*/
+
+public interface SmartFunctions extends BasicFunctions {
+}
