5dc25991a4c8c199a8cbbf795d763605fa9ae1b1
Tenevyh <tenevyh@gmail.com>
2021-11-13 12:30:13 +0300

    Create JavarushQuest.java

---
+++ JavaR/14lvl/Task14.16/JavarushQuest.java
@@ -0,0 +1,13 @@
+/*
+Перечислим квесты Javarush
+*/
+
+public enum JavarushQuest {
+    JAVA_SYNTAX,
+    JAVA_CORE,
+    JAVA_MULTITHREADING,
+    JAVA_COLLECTIONS,
+    CS_50,
+    ANDROID,
+    GAMES
+}
