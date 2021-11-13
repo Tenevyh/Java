6bb8360821b7ac65799e553bdf19f9142a032395
Tenevyh <tenevyh@gmail.com>
2021-11-13 12:30:16 +0300

    Create Solution.java

---
+++ JavaR/14lvl/Task14.16/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        JavarushQuest [] array= JavarushQuest.values();
+        for (JavarushQuest list:array){
+            System.out.println(list.ordinal());
+        }
+    }
+}
