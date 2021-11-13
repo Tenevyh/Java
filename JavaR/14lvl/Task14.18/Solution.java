a016daa0afae48c0c2100b00482dbb80f6c680fb
Tenevyh <tenevyh@gmail.com>
2021-11-13 12:30:23 +0300

    Create Solution.java

---
+++ JavaR/14lvl/Task14.18/Solution.java
@@ -0,0 +1,23 @@
+/*
+Следующий месяц, пожалуйста
+В классе Solution реализуй метод getNextMonth, который должен возвращать следующий месяц, а если передан последний месяц, возвращать первый. Используй методы values и ordinal.
+Метод main не участвует в проверке.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getNextMonth(Month.JANUARY));
+        System.out.println(getNextMonth(Month.JULY));
+    }
+
+    public static Month getNextMonth(Month month) {
+        int index= month.ordinal();
+        if (index==11){
+            Month nextMonth=Month.values()[0];
+            return nextMonth;
+        }
+        Month nextMonth=Month.values()[index+1];
+        return nextMonth;
+    }
+}
