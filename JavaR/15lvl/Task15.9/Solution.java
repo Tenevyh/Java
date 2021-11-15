54810cbce13d1a393f5dd8dd8162a0f3ea49aef9
Tenevyh <tenevyh@gmail.com>
2021-11-15 21:41:31 +0300

    Create Solution.java

---
+++ JavaR/15lvl/Task15.9/Solution.java
@@ -0,0 +1,26 @@
+
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystemException;
+
+/* 
+Оборачивание исключений
+В методе copyFile перехвати исключения, которые бросают методы readFile и writeFile.
+Перехваченные исключения оберни в RuntimeException и пробрось дальше.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        copyFile("book.txt", "book_final_copy.txt");
+        copyFile("book_final_copy.txt", "book_last_copy.txt");
+    }
+
+    static void copyFile(String sourceFile, String destinationFile) {
+        try {
+            FileUtils.readFile(sourceFile);
+            FileUtils.writeFile(destinationFile);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
