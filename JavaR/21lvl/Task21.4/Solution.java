0ca07c3d255a3c35bb2f29c4be6dbecc26fdcbc4
Tenevyh <tenevyh@gmail.com>
2021-12-07 22:17:21 +0300

    Create Solution.java

---
+++ JavaR/21lvl/Task21.4/Solution.java
@@ -0,0 +1,26 @@
+/*
+Хороши работнички
+Напиши четыре класса: Employee (сотрудник), Manager (управляющий), Chief (директор) и Secretary (секретарь).
+Унаследуй управляющего, директора и секретаря от сотрудника.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee {
+
+    }
+}
