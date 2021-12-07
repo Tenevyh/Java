582fea397c79eac9ed8a722e782e7f6394f42079
Tenevyh <tenevyh@gmail.com>
2021-12-07 22:17:19 +0300

    Create Solution.java

---
+++ JavaR/21lvl/Task21.3/Solution.java
@@ -0,0 +1,35 @@
+/*
+Межпланетная ферма
+Напиши шесть классов: Animal (животное), Cow (корова), Pig (свинья), Sheep (овца), Bull (бык) и Goat (козел).
+Унаследуй корову, свинью, овцу, быка и козла от животного.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Animal {
+
+    }
+
+    public class Cow extends Animal{
+
+    }
+
+    public class Pig extends Animal {
+
+    }
+
+    public class Sheep extends Animal {
+
+    }
+
+    public class Bull extends Animal {
+
+    }
+
+    public class Goat extends Animal {
+
+    }
+
+}
