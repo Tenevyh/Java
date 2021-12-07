32d709be5e69ac607a93ab37a51da1933cdd5772
Tenevyh <tenevyh@gmail.com>
2021-12-07 22:17:16 +0300

    Create Solution.java

---
+++ JavaR/21lvl/Task21.2/Solution.java
@@ -0,0 +1,24 @@
+/*
+Домашние животные
+Отредактируй три класса: Pet (домашнее животное), Cat (кот) и Dog (собака).
+Унаследуй кота и собаку от животного.
+
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
