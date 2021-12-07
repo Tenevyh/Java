42bb748940d64ac3aae06faa1f09fbc809972a45
Tenevyh <tenevyh@gmail.com>
2021-12-07 22:17:13 +0300

    Create Solution.java

---
+++ JavaR/21lvl/Task21.1/Solution.java
@@ -0,0 +1,19 @@
+
+/*
+Лошадь и Пегас
+Отредактируй два класса — Horse (лошадь) и Pegasus (пегас).
+Унаследуй пегаса от лошади.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse {
+
+    }
+}
