e6bbd7a5ab2eb927b3cd83be42d9459007b22fa8
Tenevyh <tenevyh@gmail.com>
2021-12-16 20:28:14 +0300

    Create Solution.java

---
+++ JavaR/22lvl/Task22.9/Solution.java
@@ -0,0 +1,23 @@
+/* 
+Три метода и минимум
+Напиши public static методы: int min(int, int), long min(long, long), double min(double, double).
+Каждый метод должен возвращать минимальное из двух переданных в него чисел.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int a, int b){
+        return a < b? a : b;
+    }
+
+    public static long min(long a, long b){
+        return a < b? a : b;
+    }
+
+    public static double min(double a, double b){
+        return a < b? a : b;
+    }
+}
