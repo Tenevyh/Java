c4dd8b351997e11e919b16275032a86435da4645
Tenevyh <tenevyh@gmail.com>
2021-12-16 20:28:03 +0300

    Create Solution.java

---
+++ JavaR/22lvl/Task22.11/Solution.java
@@ -0,0 +1,17 @@
+/* 
+Абстрактный класс Pet
+Сделай класс Pet абстрактным.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+    }
+
+}
