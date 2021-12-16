2a6ab77e633f3d8e6d1ca9641a2cff28fa786d66
Tenevyh <tenevyh@gmail.com>
2021-12-16 20:28:12 +0300

    Create Solution.java

---
+++ JavaR/22lvl/Task22.8/Solution.java
@@ -0,0 +1,18 @@
+/* 
+Свобода печати
+Напиши пять методов print() с разными параметрами.
+
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int x){}
+    public static void print(String a){}
+    public static void print(long a){}
+    public static void print(byte a){}
+    public static void print(short v){}
+}
