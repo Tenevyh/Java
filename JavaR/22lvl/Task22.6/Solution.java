4384fe2b085f9cda61216e0892155a9ac710a935
Tenevyh <tenevyh@gmail.com>
2021-12-16 20:28:06 +0300

    Create Solution.java

---
+++ JavaR/22lvl/Task22.6/Solution.java
@@ -0,0 +1,17 @@
+/* 
+Выполняем перегрузку!
+Напиши два метода: print(int) и print(String).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String print(String a){
+        return a;
+    }
+    public static int print(int a){
+        return a;
+    }
+}
