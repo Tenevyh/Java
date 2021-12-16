1edbb3237155b7d549607c2d5d4a58234ead8f80
Tenevyh <tenevyh@gmail.com>
2021-12-16 20:28:09 +0300

    Create Solution.java

---
+++ JavaR/22lvl/Task22.7/Solution.java
@@ -0,0 +1,18 @@
+/* 
+Int и Integer
+Реализуй два метода: print(int) и print(Integer).
+Напиши такой код в методе main(), чтобы вызвались оба.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    print(5);
+    print((Integer) 4);
+    }
+
+    public static void print(Integer x){
+    }
+
+    public static void print(int x){
+    }
+}
