ccd8f24364bb4b66fa4d7167561d88046d071c7c
Tenevyh <tenevyh@gmail.com>
2021-12-18 23:03:32 +0300

    Create Solution.java

---
+++ JavaR/22lvl/Task22.13/Solution.java
@@ -0,0 +1,21 @@
+/* 
+«Исправь код», часть 2
+Исправь код, чтобы программа компилировалась.
+
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+
+        public abstract Pet getChild();
+    }
+
+}
