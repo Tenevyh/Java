8efb96fc96ee2cd354e7a6744ec0019073a62010
Tenevyh <tenevyh@gmail.com>
2021-12-18 23:03:27 +0300

    Create Solution.java

---
+++ JavaR/22lvl/Task22.12/Solution.java
@@ -0,0 +1,20 @@
+/* 
+«Исправь код», часть 1
+Исправь код, чтобы программа компилировалась.
+
+Подсказка: метод getChild должен остаться абстрактным.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+
+        public abstract Pet getChild();
+    }
+}
