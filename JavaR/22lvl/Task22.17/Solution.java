0432f66f415f3dc683689cb544c5ee7de402c0dd
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2021-12-20 23:18:32 +0300

    Create Solution.java

---
+++ JavaR/22lvl/Task22.17/Solution.java
@@ -0,0 +1,21 @@
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly{
+        public void fly();
+    }
+
+    public interface CanRun{
+        public void run();
+    }
+
+    public interface CanSwim{
+        public void swim();
+    }
+}
