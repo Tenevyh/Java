92ebfdab011545e3ec9bf5cfb47468d3fcc6644e
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2021-12-22 01:29:28 +0300

    Create Solution.java

---
+++ JavaR/22lvl/Task22.29/Solution.java
@@ -0,0 +1,25 @@
+/* 
+Родитель класса CTO
+Добавь такой класс-родитель к классу CTO (технический директор), чтобы класс перестал быть абстрактным.
+Добавлять/реализовывать методы в классе CTO запрещается.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+    public static class businessman{
+        public void workHard(){
+            workHard();
+        }
+    }
+    public static class CTO extends businessman implements Businessman {
+
+    }
+}
