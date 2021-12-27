4d74285df3fa0d9dce3c26d3a794281c6786487e
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2021-12-28 01:24:21 +0300

    Create Robot.java

---
+++ JavaR/23lvl/Task23.28/Robot.java
@@ -0,0 +1,12 @@
+public class Robot extends AbstractRobot {
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
