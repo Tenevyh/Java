7304f4cc9df29dc8deb4f0f427f18391c4b5e5fc
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2021-12-28 01:23:54 +0300

    Create Solution.java

---
+++ JavaR/23lvl/Task23.24/Solution.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+/* 
+Один метод в классе
+Реализуй интерфейс Animal в классе Fox.
+Поменяй код так, чтобы в классе Fox был только один метод - getName.
+Учти, что методы удалять нельзя.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+
+        Integer getAge();
+    }
+
+    public static abstract class Fox implements Animal {
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
