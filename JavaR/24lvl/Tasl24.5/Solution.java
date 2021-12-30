e1c93fc58a6e43a5b4be0dbfb5632933116fe2df
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2021-12-30 21:05:31 +0300

    Create Solution.java

---
+++ JavaR/24lvl/Tasl24.5/Solution.java
@@ -0,0 +1,43 @@
+/* 
+Пора покушать
+Давай напишем программу, которая поможет тебе выбрать, что съесть на обед.
+Для этого нужно:
+Реализовать интерфейс Selectable в классе Food.
+Метод onSelect() должен выводить на экран фразу "The food was selected".
+Подумай, какие методы можно вызвать для переменной food, а какие для — selectable.
+В методе callFoodMethods вызови методы onSelect, onEat, если это возможно.
+В методе callSelectableMethods вызови методы onSelect, onEat, если это возможно.
+Не используй явное приведение типов.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Food food = new Food();
+        Selectable selectable = new Food();
+
+        callFoodMethods(food);
+        callSelectableMethods(selectable);
+    }
+
+    public static void callFoodMethods(Food food) {
+        food.onEat();
+        food.onSelect();
+    }
+
+    public static void callSelectableMethods(Selectable selectable) {
+        selectable.onSelect();
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    static class Food implements Selectable {
+        public void onSelect(){
+            System.out.println("The food was selected");
+        }
+        public void onEat() {
+            System.out.println("The food was eaten");
+        }
+    }
+}
