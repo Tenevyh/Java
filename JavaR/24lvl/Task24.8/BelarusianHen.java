a60af228c0eddda538d44590d7e1b3d1af91b3b0
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2021-12-31 19:45:47 +0300

    Create BelarusianHen.java

---
+++ JavaR/24lvl/Task24.8/BelarusianHen.java
@@ -0,0 +1,8 @@
+public class BelarusianHen extends Hen{
+    public int getCountOfEggsPerMonth(){
+        return 12;
+    }
+    public String getDescription(){
+        return super.getDescription() + String.format(" Моя страна - %s. Я несу %d яиц в месяц.", Country.BELARUS, getCountOfEggsPerMonth());
+    }
+}
