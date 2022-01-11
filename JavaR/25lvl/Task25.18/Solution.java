51a83635138f551c0e13c019df733316fb82977d
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-01-12 01:45:18 +0300

    Create Solution.java

---
+++ JavaR/25lvl/Task25.18/Solution.java
@@ -0,0 +1,25 @@
+/* 
+Статики и котики
+В этой задаче тебе нужно:
+Создать класс Cat с публичным полем name типа String в классе Solution.
+В статическом блоке создать объект типа Cat и присвоить его переменной cat (не забудь инициализировать поле name).
+В статическом блоке вывести имя созданного кота на экран.
+Примечание: не создавай конструкторы с параметрами в классе Cat.
+*/
+
+public class Solution {
+    public static Cat cat;
+    static {
+        cat=new Cat();
+        cat.name="Franko";
+        System.out.println(cat.name);
+
+    }
+    public static void main(String[] args) {
+
+    }
+
+    public static class Cat{
+        public String name;
+    }
+}
