9ffa3cd7e1f66396e3782cb0b0dfa94d3c1b2271
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-01-15 17:27:38 +0300

    Create DrinkMaker.java

---
+++ JavaR/25lvl/Task25.20/DrinkMaker.java
@@ -0,0 +1,11 @@
+public abstract class DrinkMaker {
+    public abstract void getRightCup();
+    public abstract void putIngredient();
+    public abstract void pour();
+
+    public void makeDrink(){
+        getRightCup();
+        putIngredient();
+        pour();
+    }
+}
