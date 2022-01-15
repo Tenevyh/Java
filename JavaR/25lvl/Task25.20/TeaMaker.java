62944a2b4c6adfc6948abacfed80acf635b0cd0a
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-01-15 17:27:44 +0300

    Create TeaMaker.java

---
+++ JavaR/25lvl/Task25.20/TeaMaker.java
@@ -0,0 +1,11 @@
+public class TeaMaker extends DrinkMaker{
+    public void getRightCup(){
+        System.out.println("Берем чашку для чая");
+    }
+    public void putIngredient(){
+        System.out.println("Насыпаем чай");
+    }
+    public void pour(){
+        System.out.println("Заливаем кипятком");
+    }
+}
