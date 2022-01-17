2a931fd0af7af5ed6d43194d1c661536f61d43ea
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-01-17 20:42:32 +0300

    Create Sun.java

---
+++ JavaR/25lvl/Task25.25/Sun.java
@@ -0,0 +1,11 @@
+public class Sun implements Planet{
+    private static Sun instance;
+    private Sun(){}
+    public static Sun getInstance(){
+        if(instance==null){
+            instance=new Sun();
+        }
+        return instance;
+    }
+
+}
