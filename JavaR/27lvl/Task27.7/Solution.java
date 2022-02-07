5f4774caa1e04ad0221a4d8768f41e927730c2f3
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-02-07 20:37:29 +0300

    Create Solution.java

---
+++ JavaR/27lvl/Task27.7/Solution.java
@@ -0,0 +1,11 @@
+/* 
+МВФ
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        IMF fund = IMF.getFund();
+        IMF anotherFund = IMF.getFund();
+        System.out.println(fund == anotherFund);
+    }
+}
