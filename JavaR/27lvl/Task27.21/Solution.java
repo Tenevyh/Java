afe2822c7b9e64684b5487a0a355aa026e5c0616
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-02-17 20:51:49 +0300

    Create Solution.java

---
+++ JavaR/27lvl/Task27.21/Solution.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Транзакционность
+Сделать метод joinData транзакционным, т.е. если произошел сбой, то данные не должны быть изменены.
+1. Считать с консоли 2 имени файла.
+2. Считать построчно данные из файлов. Из первого файла - в allLines, из второго - в forRemoveLines.
+В методе joinData:
+3. Если список allLines содержит все строки из forRemoveLines, то удалить из списка allLines все строки, которые есть в forRemoveLines.
+4. Если условие из п.3 не выполнено, то:
+4.1. очистить allLines от данных
+4.2. выбросить исключение CorruptedDataException
+Метод joinData должен вызываться в main. Все исключения обработайте в методе main.
+Не забудь закрыть потоки.
+*/
+
+public class Solution {
+    public static List<String> allLines = new ArrayList<String>();
+    public static List<String> forRemoveLines = new ArrayList<String>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        String fileName1=reader.readLine();
+        String fileName2=reader.readLine();
+        BufferedReader readFile1=new BufferedReader(new FileReader(fileName1));
+        String line1=null;
+        while ((line1=readFile1.readLine())!=null){
+            allLines.add(line1);
+        }
+        BufferedReader readFile2=new BufferedReader(new FileReader(fileName2));
+        String line2=null;
+        while ((line2=readFile2.readLine())!=null){
+            forRemoveLines.add(line2);
+        }
+        new Solution().joinData();
+        reader.close();
+        readFile1.close();
+        readFile2.close();
+    }
+
+    public void joinData() throws CorruptedDataException {
+        if (allLines.containsAll(forRemoveLines))
+            allLines.removeAll(forRemoveLines);
+        else {
+            allLines.clear();
+            throw new CorruptedDataException();
+        }
+    }
+}
