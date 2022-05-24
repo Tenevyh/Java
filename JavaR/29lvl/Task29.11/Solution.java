45c3de28947ce2f0589a1dfb5e3776854eed7bea
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-05-24 21:43:53 +0300

    Create Solution.java

---
+++ JavaR/29lvl/Task29.11/Solution.java
@@ -0,0 +1,38 @@
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+/* 
+Ридер обертка
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна преобразовывать весь текст в заглавные буквы.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
+        PrintStream stream= new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+        String result = outputStream.toString();
+
+        System.setOut(consoleStream);
+        String uppCase=result.toUpperCase();
+        
+        System.out.println(uppCase);
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
